dc39c17b182cc4facedf0e3268de7abe02dc1fa2
Mwenda <mwendakanana298@gmail.com>
2023-02-19 05:15:18 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combinations of single-digit numbers
+ * Return: Always 0 (Pass)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 23; n < 43; n++)
+	{
+		putchar(n);
+		if (n != 44)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}

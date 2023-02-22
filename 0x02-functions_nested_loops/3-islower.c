fa3a3fd1606b874e70d61c6936577977cc7ffc53
Mwenda <mwendakanana298@gmail.com>
2023-02-22 14:50:08 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_is lower - check if char is lowercase
+ *@c: is char to be checked
+ *@_islower: char checked
+ * Return: 1 if char is lowercase, otherwise 0
+ */
+
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}

b9458cb1d4a54487c6b46c00c828e0a5bb93334e
Mwenda <mwendakanana298@gmail.com>
2023-03-14 13:27:44 -0700

    0-create_array.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,15 +1,14 @@
-#include <stdio.h>		
+#include <stdio.h>	
 #include <stdlib.h>
 #include "main.h"
-		
-/**		
- * _strdup - duplicate to new memory space location		
- * @str: char		
- * Return: 0		
-*/		
-char *_strdup(char *str)		
-{		
-char *aaa;
+/**
+ *_strdup - duplicate to new memory space location		
+ *@str: char		
+ *Return: 0
+*/ 
+char*_strdup(char *str)		
+{	
+char*aaa;
 int i, r = 0;
 if (str == NULL)
 return (NULL);
